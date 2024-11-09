b7204cffa643ea64288aeb834f34687af1a6087e
DEEPAK KUMAR PRABHAT <130199914+DKPCODER@users.noreply.github.com>
2024-11-09 15:55:21 +0530

    shoppy

---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+
+const Hero=()=>{
+    return (
+        <main className ="hero container">
+            <div className = "hero-content">
+                <h1>YOUR FEET DESERVE THE BEST ONE</h1>
+                <p>
+                    YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH OUR SHOES , YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH OUR SHOES.
+                </p>
+                <div className = "hero-btn">
+                    <button className="bt1">Shop Now</button>
+                    <button className="bt3">Category</button>
+                </div>
+                <div className="shpy">
+                    <p>Available Now</p>
+                    <div className="flip">
+                    <img src="/images/flipkart.png" alt="flip"/>
+                    <img src="/images/amazon.png" alt="amazon"/>
+                    </div>
+                
+                </div>
+                
+
+            </div>
+            <div className="hero-image">
+                <img src="/images/shoe_image.png" alt="hero"/>
+            </div>
+           
+
+        </main>
+    )
+
+};
+export default Hero;
